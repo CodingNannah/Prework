54bae37eac9a67351fb56566ef06379c89d1ec35
Diana - Coding Nannah <107065069+CodingNannah@users.noreply.github.com>
2022-07-22 14:25:05 -0600

    Adding corrections to resolve VSC error flags

--- coding_questions.py
+++ coding_questions.py
@@ -16,7 +16,7 @@ hello_name("mr. president")
 # from 1-100 and returns nothing
 
 def first_odds():
-    for num in range (1, 100, 2):
+    for num in range (1, 101, 2):
         print(num)
 
 first_odds()
@@ -25,10 +25,13 @@ first_odds()
 
 # Question 3
 # Write a function, max_num_in_list to return the max number of a given list.
+# start with 1st spot in a list = [0]
+# this will rewrite the max number as the number to compare with next: if num > max: max= num (put on TWO lines! & check indent)
+# not print! use return - check indent  
 # The first line of code has been defined as below:
 
 def max_num_in_list(a_list):
-#start with 1st spot in a list = [0]
+
     max = a_list[0]
     for num in a_list:
         if num > max:
@@ -44,32 +47,35 @@ max_num_in_list([1, 7, 12, 5, 365, 14, 6, 18])
 # divisible by 4 but not by 100, unless it is also divisible by 400.
 # The return should be boolean Type (true/false).
 
-def is_leap_year(a_year):
-    if a_year % 4 == 0 and a_year % 100 != 0:
-    elif a_year % 400 == 0:
-         return True
+def leap_year(year):
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    elif year % 400 == 0:
+        return True
     else:
         return False
     
-is_leap_year(2000) 
-is_leap_year(1989)
-is_leap_year(2222)
+leap_year(2000) 
+leap_year(1989)
+leap_year(2222)
 
 # SyntaxError with elif
 # missing something! 
+# I think I fixed it by adding return True under the if
 
 
 
 # Question 5
 # Write a function to see if all numbers in a list are consecutive. 
-# # The return should be boolean Type.
+# Hint: Transitive property of addition. If a number is equal to the next number, then...
+# The return should be boolean Type. = T or F
 
-def is_consecutive(a_list):
+def consecutive_nums(a_list):
     for num in a_list:
-        if a_list[0]+1 == a_list[0+1]:
+        if a_list[0]+1 == a_list([0]+1):
             return True
         else:
             return False
         
-is_consecutive([1,2,3,4,5,6,7])
-is_consecutive([3,6,9,12,15,18])
+consecutive_nums([1,2,3,4,5,6,7])
+consecutive_nums([3,6,9,12,15,18])
